2bae615248aa4f4e224f0992ec3ef7a39c8823ce
Adailson <adailson2@gmail.com>
2019-06-05 19:46:02 -0300

    Gráfico de tempo Vermelho e Preto

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 from red_black_tree.rbt import RedBlackTree
 import os, random, time
 number = 1
@@ -7,6 +9,14 @@ try:
     tree = RedBlackTree()
 except:
     print('Exception occurred')
+
+def plot_graph(time,n):
+    plt.ylabel('Tempo para %d nós (em milisegundos)' % (n))
+    
+    for key in time.keys():
+        plt.bar(key, time[key])
+
+    plt.show()
 
 def print_tree(node):
     if node.value == None :
@@ -24,6 +34,8 @@ def get_values(n):
     print(values)
     return values
 
+results = {'RB': []}
+result = {'RB': 0}
 while(number):
     print("Escolha uma das opções:\n1- Inserir valores randômicos para n nós.\n0- Para sair\n")
     number = int(input())
@@ -35,15 +47,15 @@ while(number):
         for value in values:
             tree.add(value)
         end = time.time()
-        diff = end - start
+        diff = float(end - start)
+        diff = diff * 1000
         root = tree.root
         print('O tempo de inseção de %d nós foi de %f segundos ' % (n, diff))
+        for key in result.keys():
+            results[key].append(diff)
         print_tree(root)
+        plot_graph(results, n)
 
-    print("Aperte enter para continuar!!!")
-    try:
-        input()
-    except:
-        print("Você está no python2 por favor aperte enter de novo")
-        raw_input()
+    print("Aperte enter para continuar!")
+    input()
     os.system('clear')
