7fe8e71dccadedc81d1df43489a80b6279a350d2
Adailson <adailson2@gmail.com>
2019-06-07 16:15:56 -0300

    Comparativo de remoção e refatoração de código

--- main.py
+++ main.py
@@ -38,6 +38,8 @@ results_insert = {'RB': [], 'AVL': []}
 result_insert = {'RB': 0, 'AVL': 0}
 results_search = {'RB': [], 'AVL': []}
 result_search = {'RB': 0, 'AVL': 0}
+results_remove = {'RB': [], 'AVL': []}
+result_remove = {'RB': 0, 'AVL': 0}
 
 while(number):
     start = 0
@@ -47,71 +49,93 @@ while(number):
     value_sought = 0
     diff = 0
     i = 0
-    print("Escolha uma das opções:\n1- Comparativo inserção (insere valores randômicos para n nós)\n2- Comparativo de busca (busca valor randômico em valores para n nós) \n0- Para sair\n")
+    print("Escolha uma das opções:\n"
+        +"1- Comparativo inserção AVL x Red-Black (insere valores randômicos para n nós)\n"
+        +"2- Comparativo de busca (busca valor randômico em valores para n nós)\n"
+        +"3- Comparativo de remoção\n"
+        +"0- Para sair\n")
     number = int(input())
-    if number == 1 or number == 2:
+    if number == 1 or number == 2 or number == 3:
         print("Digite a quantidade de nós:")
         n = int(input())
         values = get_values(n)
         if number == 1:
             print("Digite quantas iterações você deseja inserir para %d nós:" % n)
             b = int(input())
-        else:
+        elif number == 2:
             print("Digite quantas iterações você deseja buscar para %d nós:" % n)
             b = int(input())
-        if number == 1:
-            for key in result_insert.keys():
-                if key == 'AVL': 
-                    avl = AVL_Tree()
-                    root = None
+        else:
+            print("Digite quantas iterações você deseja remover para %d nós:" % n)
+            b = int(input())
+        # if number == 1 or number == 3:
+        for key in result_insert.keys():
+            if key == 'AVL': 
+                avl = AVL_Tree()
+                root = None
+                if number == 1:
                     start = time.time()
                     for i in range(0,b):
                         for value in values:
                             root = avl.insert(root, value)
-                elif key == 'RB':
-                    start = time.time()
-                    for i in range(0,b):
-                        for value in values:
-                            rbt.add(value)
-                    root = rbt.root
-                
-                end = time.time()
-                diff = float(end - start)
-                diff = diff * 1000
-                
-                result_insert[key] = diff
-                results_insert[key].append(diff)
-            
-            comparation_graph(results_insert, n)
-        else:
-            for key in result_insert.keys():
-                if key == 'AVL':
-                    avl = AVL_Tree()
-                    root = None
+                elif number == 2:
                     for value in values:
                         root = avl.insert(root, value)                    
                     value_sought = random.choice(values)
                     start = time.time()
                     for i in range(0,b):
-                        avl.find_node(root, value_sought)
+                        avl.find_node(root, value_sought)    
+                else:
+                    for value in values:
+                        root = avl.insert(root, value)
+                    start = time.time()
+                    for i in range(0,b):
+                        for value in values:
+                            root = avl.delete(root, value)
 
-                elif key == 'RB':
+            elif key == 'RB':
+                if number == 1:
+                    start = time.time()
+                    for i in range(0,b):
+                        for value in values:
+                            rbt.add(value)
+                    root = rbt.root
+                elif number == 2:
                     for value in values:
                         rbt.add(value)                    
                     value_sought = random.choice(values)
                     start = time.time()
                     for i in range(0,b):
                         rbt.find_node(value_sought)
+                else:
+                    start = time.time()
+                    for value in values:
+                        rbt.add(value)
+                    root = rbt.root
+                    for i in range(0,b):
+                        for value in values:
+                            rbt.remove(value)
 
-                end = time.time()
-                diff = float(end - start)
-                diff = diff * 1000
-                
+            end = time.time()
+            diff = float(end - start)
+            diff = diff * 1000
+            
+            if number == 1:
+                result_insert[key] = diff
+                results_insert[key].append(diff)
+            elif number == 2:
                 result_search[key] = diff
                 results_search[key].append(diff)
+            else:
+                result_remove[key] = diff
+                results_remove[key].append(diff)
 
-            root = rbt.root
-            comparation_graph(results_search, n)
+        if number == 1:
+            comparation_graph(results_insert, n)
+        elif number == 2:
+            comparation_graph(results_search, n)    
+        else:
+            comparation_graph(results_remove, n) 
 
     print("Aperte enter para continuar!")
     input()
